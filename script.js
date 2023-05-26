950ec7b0b08fd658fd94dff5aa21d9f7fa907e45
reocchi <reo700+github@gmail.com>
2023-05-26 14:49:53 +1000

    Added files

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ var nav = document.getElementsByClassName('hamburger')[0];
 
 navMenu.addEventListener('click', function () {
 
-    nav.classList.toggle(' is_active');
+    nav.classList.toggle('is_active');
     console.log('Hello!!');
 }, false);
