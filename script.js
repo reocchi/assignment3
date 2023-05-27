d92e45af67eba2a0694b05d0924cdc52129cc479
reocchi <reo700+github@gmail.com>
2023-05-27 14:56:39 +1000

    adjusted some pages for 1024px resolution

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
+// Toggle Menu
 var navMenu = document.getElementsByClassName('hamburger_menu')[0];
-
 var nav = document.getElementsByClassName('hamburger')[0];
-
 navMenu.addEventListener('click', function () {
 
     nav.classList.toggle('is_active');
-    console.log('Hello!!');
 }, false);
 
-var cartLink = document.getElementsByClassName('cart_link')[0];
 
+// Change change the link of 'Add to Cart' on desktop
+var cartLink = document.getElementsByClassName('cart_link')[0];
 if (window.screen.width >= 490) {
     console.log('above 490');
     document.getElementsByClassName('cart_link')[0].href = "order.html";
 }
+
+// Change the quantity
+
+var plusBtn = document.getElementsByClassName('plus')[0];
+var minusBtn = document.getElementsByClassName('minus')[0];
+
+var orderQty = document.getElementsByClassName('quantity')[0];
+
+const state = { count: 0 };
+
+plusBtn.addEventListener('click', function (){
+    console.log('clicked!');
+    orderQty.innerHTML  = ++state.count;
+
+    
+}, false);
+
+minusBtn.addEventListener('click', function (){
+    console.log('clicked!');
+    if (state.count > 0) {
+       orderQty.innerHTML  = --state.count; 
+    }
+    
+}, false);
